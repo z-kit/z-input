9025e023645a2654f0a643ac8d1732d8d57e95dd
Matheus Kautzmann <kautzmann5@gmail.com>
2017-03-04 17:19:33 -0300

    Migrate nightwatch -> nightmare

--- test/regression/suite/component.js
+++ test/regression/suite/component.js
@@ -1,94 +1,186 @@
-module.exports = {
-  default: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
-      .waitForElementPresent('.z-input', 1000)
-      .assert.attributeContains('.z-input > input', 'placeholder', 'Type something')
-      .getCssProperty('.z-input > input', 'border-bottom', (result) => {
-        browser.assert.equal(result.value, '2px solid rgb(170, 170, 170)');
-      })
-      .end();
-  },
-  box: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=box')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input > input', 'box-shadow', (result) => {
-        browser.assert.equal(result.value === 'none', false);
-      })
-      .end();
-  },
-  borderless: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=borderless')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input > input', 'border', (result) => {
-        browser.assert.equal(result.value, '0px none rgb(0, 0, 0)');
-      })
-      .end();
-  },
-  label: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=label')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input__label', 'top', (result) => {
-        browser.assert.equal(result.value, '4px');
-      })
-      .click('.z-input > input')
-      .getCssProperty('.z-input__label', 'top', (result) => {
-        browser.assert.equal(Math.floor(parseInt(result.value, 10)) < 4, true);
-      })
-      .end();
-  },
-  colors: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=colors')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input:nth-of-type(1) > input', 'border-color', (result) => {
-        browser.assert.equal(result.value, 'rgb(118, 178, 69)');
-      })
-      .getCssProperty('.z-input:nth-of-type(2) > input', 'border-color', (result) => {
-        browser.assert.equal(result.value, 'rgb(254, 215, 102)');
-      })
-      .getCssProperty('.z-input:nth-of-type(3) > input', 'border-color', (result) => {
-        browser.assert.equal(result.value, 'rgb(240, 58, 71)');
-      })
-      .getCssProperty('.z-input:nth-of-type(4) > input', 'border-color', (result) => {
-        browser.assert.equal(result.value, 'rgb(255, 0, 0)');
-      })
-      .end();
-  },
-  'prefix and suffix': (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=prefix%20and%20suffix')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input > input', 'padding', (result) => {
-        browser.assert.equal(result.value, '0px 34px');
-      })
-      .getCssProperty('.z-input__prefix', 'left', (result) => {
-        browser.assert.equal(result.value, '0px');
-      })
-      .getCssProperty('.z-input__suffix', 'right', (result) => {
-        browser.assert.equal(result.value, '0px');
-      })
-      .end();
-  },
-  'disabled input': (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=disabled%20input')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input > input', 'opacity', (result) => {
-        browser.assert.equal(result.value, '0,5');
-      })
-      .end();
-  },
-  dark: (browser) => {
-    browser
-      .url('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=dark')
-      .waitForElementPresent('.z-input', 1000)
-      .getCssProperty('.z-input > input', 'color', (result) => {
-        browser.assert.equal(result.value, 'rgba(255, 255, 255, 1)');
-      })
-      .end();
-  },
-};
+import { test } from 'ava';
+import Nightmare from 'nightmare';
+
+test('CSS component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render the placeholder';
+  const expected = 'Type something';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
+    .wait('.z-input')
+    .evaluate(() => document.querySelector('.z-input > input').getAttribute('placeholder'))
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - default', (t) => {
+  t.plan(1);
+  const msg = 'should render the bottom border';
+  const expected = '2px solid rgb(170, 170, 170)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input > input')).borderBottom)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - box', (t) => {
+  t.plan(1);
+  const msg = 'should render shadows on box input';
+  const notExpected = 'none';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=box')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input > input')).boxShadow)
+    .end()
+    .then(actual => t.notDeepEqual(notExpected, actual, msg));
+});
+
+test('CSS component - borderless', (t) => {
+  t.plan(1);
+  const msg = 'should render borderless input';
+  const expected = '0px none rgb(0, 0, 0)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=borderless')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input > input')).border)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - label', (t) => {
+  t.plan(1);
+  const msg = 'should render the label at the correct position';
+  const expected = '4px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=label')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input__label')).top)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - label', (t) => {
+  t.plan(1);
+  const msg = 'should float the label';
+  const expected = '-20px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=label')
+    .wait('.z-input')
+    .type('.z-input > input', 'Test')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input__label')).top)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render the success color';
+  const expected = 'rgb(118, 178, 69)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=colors')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input:nth-of-type(1) > input')).borderColor)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render the warning color';
+  const expected = 'rgb(254, 215, 102)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=colors')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input:nth-of-type(2) > input')).borderColor)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render the danger color';
+  const expected = 'rgb(240, 58, 71)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=colors')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input:nth-of-type(3) > input')).borderColor)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - colors', (t) => {
+  t.plan(1);
+  const msg = 'should render the error color';
+  const expected = 'rgb(255, 0, 0)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=colors')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input:nth-of-type(4) > input')).borderColor)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - prefix and suffix', (t) => {
+  t.plan(1);
+  const msg = 'should render the input padding to make room for prefixes and suffixes';
+  const expected = '0px 34px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=prefix%20and%20suffix')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input > input')).padding)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - prefix and suffix', (t) => {
+  t.plan(1);
+  const msg = 'should render the prefix on the left';
+  const expected = '0px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=prefix%20and%20suffix')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input__prefix')).left)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - prefix and suffix', (t) => {
+  t.plan(1);
+  const msg = 'should render the suffix on the right';
+  const expected = '0px';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=prefix%20and%20suffix')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input__suffix')).right)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - disabled input', (t) => {
+  t.plan(1);
+  const msg = 'should render an input with some opacity';
+  const expected = 0;
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=disabled%20input')
+    .wait('.z-input')
+    .evaluate(() => {
+      const inputStyle = getComputedStyle(document.querySelector('.z-input > input'));
+      return parseInt(inputStyle.opacity, 10);
+    })
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
+
+test('CSS component - dark', (t) => {
+  t.plan(1);
+  const msg = 'should render an input with white text color';
+  const expected = 'rgb(255, 255, 255)';
+  return Nightmare()
+    .goto('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=dark')
+    .wait('.z-input')
+    .evaluate(() => getComputedStyle(document.querySelector('.z-input > input')).color)
+    .end()
+    .then(actual => t.deepEqual(expected, actual, msg));
+});
